cc63a3fa238b2156b51dd203b2be85a31fbcb659
Vitor Lee <vitorleehan123@gmail.com>
2021-02-26 08:02:47 -0300

    Update TwitterConnection.py

--- twittpost/TwitterConnection.py
+++ twittpost/TwitterConnection.py
@@ -28,20 +28,20 @@ import urllib.parse
 
 class Twitter: 
     def __init__(self, api_key, secret_key, token_key, token_secret): 
-        """Set the keys"""
+        """Set the keys of the Twitter App"""
         self.connection(
             api_key, secret_key, token_key, token_secret)
 
     
     def connection(self, api_key, secret_key, token_key, token_secret):
-        """Connect with the keys"""
+        """Connect with the keys of the Twitter App"""
         self.consumer = oauth2.Consumer(api_key, secret_key) 
         self.token = oauth2.Token(token_key, token_secret)
         self.cliente = oauth2.Client(self.consumer, self.token)
 
 
     def tweet(self, new_tweet): 
-        """Post a tweet using the terminal
+        """Post a tweet using the terminal:
         - Put the method = POST, to indicate the type of method
         - Get the api of statuses/update """
         query_codificate = urllib.parse.quote(new_tweet, safe='')
@@ -56,7 +56,7 @@ class Twitter:
 
     
     def search(self, query, lang):
-        """Search for tweets.
+        """Searching for tweets:
         - Get the api of search/tweets 
         - lang = the lang of the search 
         - twitts = objeto['statuses'] - just get the statuses """
