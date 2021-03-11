cfa5bd8b03f444d6dd04f18fedb2aa13c9d65c84
vLeeH <vitorleehan123@gmail.com>
2021-03-10 23:10:39 -0300

    Update

--- examples/post-log.py
+++ examples/post-log.py
@@ -14,10 +14,6 @@ import os
 import pprint
 from time import sleep
 
-api_key = API_KEY
-secret_key = SECRET_KEY
-token_key = TOKEN_KEY
-token_secret = TOKEN_SECRET
 
 twitter = Twitter(api_key, secret_key, token_key, token_secret)
 
